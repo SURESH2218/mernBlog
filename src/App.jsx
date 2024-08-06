8a84ee4c432a9917f5b057a7c789cb8f91c7260b
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-08-06 17:01:42 +0530

    router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Projects from "./pages/Projects";
+
 function App() {
   return (
-    <div>
-      <p className="text-black text-2xl">suresh alabani</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes> golidke
+    </BrowserRouter>
   );
 }
 
