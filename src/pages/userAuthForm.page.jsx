3b548ff3ee0ee07ed1a742c703ea659735cbb0ea
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-08-22 22:26:26 +0530

    sign in and sign up ui designed

---
+++ src/pages/userAuthForm.page.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+const UserAuthForm = ({ type }) => {
+  return (
+    <section className="h-cover flex items-center justify-center">
+      <form className="w-[80%] max-w-[400px]">
+        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+          {type == "sign-in" ? "Welcome back" : "Join us Today"}
+        </h1>
+        {type == "sign-up" ? (
+          <InputBox
+            name="fullname"
+            type="text"
+            placeholder="Enter your full Name"
+            icon="fi-rr-user"
+          />
+        ) : (
+          ""
+        )}
+        <InputBox
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+          icon="fi-rr-envelope"
+        />
+        <InputBox
+          name="password"
+          type="password"
+          placeholder="Enter your Password"
+          icon="fi-rr-key"
+        />
+        <button className="btn-dark center mt-14">
+          {type.replace("-", " ")}
+        </button>
+        <div className="relative w-full flex items-center gap-2 my-10 opacity-30 text-black font-bold">
+          <hr className="w-1/2 border-black" />
+          <p>OR</p>
+          <hr className="w-1/2 border-black" />
+        </div>
+        <button className="btn-dark flex items-center justify-center gap-4 center w-[90%]">
+          <img src={googleIcon} className="w-5 " alt="" />
+          continue with google
+        </button>
+        {type == "sign-in" ? (
+          <p className="mt-6 text-dark-grey text-xl text-center">
+            Don't have an account ?
+            <Link to="/signup" className=" underline text-black text-xl ml-1">
+              Join Us
+            </Link>
+          </p>
+        ) : (
+          <p className="mt-6 text-dark-grey text-xl text-center">
+            Already a member ?
+            <Link to="/signin" className=" underline text-black text-xl ml-1 ">
+              Sign in here.
+            </Link>
+          </p>
+        )}
+      </form>
+    </section>
+  );
+};
+
+export default UserAuthForm;
